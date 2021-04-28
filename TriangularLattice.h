4d11caf3e0d28440bd91456db6591ff9e72fdfdc
Takumi Miyoshi <34400mt@gmail.com>
2021-04-29 04:23:15 +0900

    add the declarations of the index manipulations

--- TriangularLattice.h
+++ TriangularLattice.h
@@ -10,5 +10,12 @@ private:
 public:
 	TriangularLattice(int x, int y);
 
-
+	int getUpperLeftIndex(int index) const;
+	int getUpperRightIndex(int index) const;
+	int getLeftIndex(int index) const;
+	int getRightIndex(int index) const;
+	int getLowerLeftIndex(int index) const;
+	int getLowerRightIndex(int index) const;
 };
+
+#include "TriangularLattice.inl"
